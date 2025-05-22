ab766c9d505944d5441e573e785a57856d671042
Sahilshowkat786 <sahilshowkatsofi@gmail.com>
2025-05-22 07:54:37 +0530

    Add files via upload

---
+++ 3rdprogm.c
@@ -0,0 +1,12 @@
+// sum of three numbers
+#include<stdio.h>
+int main(){
+
+int a,b,c,sum=0;
+printf("enter 3 numbers:");
+scanf("%d%d%d",&a,&b,&c);
+sum =a+b+c;
+printf("the sum of numbers is:%d",sum);
+
+    return 0;
+}
